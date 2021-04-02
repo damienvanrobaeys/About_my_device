ada9466cff7abb0e991a90c30806291031326b17
Damien Van Robaeys <damien.vanrobaeys@gmail.com>
2021-04-02 16:03:43 +0200

    Add files via upload

--- Sources/Actions_scripts/Upload_Logs_Sharepoint.ps1
+++ Sources/Actions_scripts/Upload_Logs_Sharepoint.ps1
@@ -1,22 +1,16 @@
-# $Get_Sharepoint_Content = [xml](get-content "$current_folder\Config\Sharepoint.xml")
-# $Get_Sharepoint_Content = [xml](get-content "..\Config\Sharepoint.xml")
-# cd..
-# $Get_Sharepoint_Content = [xml](get-content ".\Config\Sharepoint.xml")
-$Get_Sharepoint_Content = [xml](get-content "D:\GRT_AboutMyComputer\SD\Sources\Config\Sharepoint.xml")
+$Get_Sharepoint_Content = [xml](get-content ".\Config\Sharepoint.xml")
 $Sharepoint_App_ID = $Get_Sharepoint_Content.Infos.App_ID
 $Sharepoint_App_Secret = $Get_Sharepoint_Content.Infos.App_Secret
 $Sharepoint_Folder = $Get_Sharepoint_Content.Infos.Folder
 $Sharepoint_Site_URL = $Get_Sharepoint_Content.Infos.Site_URL		
 
 $CompName = $env:computername
-# $Logs_Collect_Folder = "C:\Device_Logs_From" + "_$CompName"
 $Logs_Collect_Folder = "$env:temp\Device_Logs_From" + "_$CompName"
 $Logs_Collect_Folder_ZIP = "$Logs_Collect_Folder" + ".zip"
 
 $Is_Nuget_Installed = $False     
 If(!(Get-PackageProvider | where {$_.Name -eq "Nuget"}))
 	{                                         
-		Write_Log -Message_Type "SUCCESS" -Message "The package Nuget is not installed"                                                                          
 		Try
 			{
 				[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
